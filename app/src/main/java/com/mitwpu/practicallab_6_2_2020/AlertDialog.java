20c7fcc12e9371cf45d8ebe8b43985e96352c514
AlbinoB <albinobraganza21@gmail.com>
2020-02-13 16:33:26 +0530

    Android Alter Dialog

--- app/src/main/java/com/mitwpu/practicallab_6_2_2020/AlertDialog.java
+++ app/src/main/java/com/mitwpu/practicallab_6_2_2020/AlertDialog.java
@@ -1,7 +1,10 @@
 package com.mitwpu.practicallab_6_2_2020;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class AlertDialog extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class AlertDialog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_dialog);
+    }
+
+    public void btnClicked(View view){
+        android.support.v7.app.AlertDialog.Builder builder=new android.support.v7.app.AlertDialog.Builder(AlertDialog.this);
+        builder.setTitle("Alert Dialog").
+                setMessage("Are you sure,you want to continue").
+                setCancelable(false).
+                setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(AlertDialog.this, "You selected Yes", Toast.LENGTH_SHORT).show();
+                    }
+                }).
+                setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(AlertDialog.this, "You selected No", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        builder.show();
     }
 }
